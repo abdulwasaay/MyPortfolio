0c5380d8a368c6f338e82950fac000612f04b762
abdul wasay <wasaysaleem071@gmail.com>
2025-07-20 13:50:07 -0700

    my change

--- src/app/components/Portfolio/Portfolio.js
+++ src/app/components/Portfolio/Portfolio.js
@@ -29,12 +29,12 @@ export default function Portfolio() {
           <h1 className="ml-[5px] text-[40px] text-[#0ef] font-extrabold max-[365px]:text-[30px] max-[305px]:text-[25px]">Project</h1>
         </div>
         <div className=" flex justify-center ml-[30px] mr-[30px] mt-[40px]  max-[606px]:flex-col  max-[606px]:items-center">
-          <Project data={{ id: "proj4", Url: "budget.jpg", name: "Mahaana-App", text: "Worked on a large-scale real-world product developed by the company.", path: "https://portal.mahaana.com/" }} />
-          <Project data={{ id: "proj1", Url: "netflixclone.jpg", name: "Netflix-Clone", text: "My Netflix-clone website made on React.js, Tailwind css and Firebase", path: "https://neflix-clone12.vercel.app/" }} />
+          <Project data={{ id: "proj1", Url: "song.jpg", name: "Band Web App", text: "portal for Artists and Band Members to manage there releases and songs.", path: "https://airplay-direct-frontend-dev.vercel.app/" }} />
+          <Project data={{ id: "proj2", Url: "budget.jpg", name: "Bussiness Web App", text: "Worked on a large-scale real-world product developed by the company.", path: "https://portal.mahaana.com/" }} />
         </div>
         <div className=" flex justify-center ml-[30px] mr-[30px] max-[606px]:flex-col  max-[606px]:items-center">
-          <Project data={{ id: "proj2", Url: "ecomm.jpg", name: "Ecommerce-Website", text: "My Ecommerce-Website made on Next.js and Firebase with Admin panel", path: "https://my-styluxe.vercel.app/" }} />
-          <Project data={{ id: "proj3", Url: "weath.jpg", name: "Weather-App", text: "My Weather web-Application made on React.js and Third party Weather APIs", path: "https://showweath.netlify.app/" }} />
+          <Project data={{ id: "proj3", Url: "netflixclone.jpg", name: "Netflix-Clone", text: "My Netflix-clone website made on React.js, Tailwind css and Firebase", path: "https://neflix-clone12.vercel.app/" }} />
+          <Project data={{ id: "proj4", Url: "ecomm.jpg", name: "Ecommerce-Website", text: "My Ecommerce-Website made on Next.js and Firebase with Admin panel", path: "https://my-styluxe.vercel.app/" }} />
         </div>
       </div>
     </section>
